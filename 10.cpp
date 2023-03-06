788dbe0e9ef3c9705bafec59ac8a1ed6306d0d0e
xaskilaki007 <115955213+xaskilaki007@users.noreply.github.com>
2023-03-06 21:01:03 +0300

    Update 10.cpp

--- 10.cpp
+++ 10.cpp
@@ -7,20 +7,34 @@
 #include <time.h>
 using namespace std;
 int main(){
-    int minim,gl,m;
+    int minim,gl,m,n1,ni,k,y;
     srand(time(0));
     std::array<std::array<int, 5>, 5>mat;
-    for(int T=0;T<5;T++){
-        for(int U=0;U<5;U++){
+    for(int T=0;T<5;T++)
+        for(int U=0;U<5;U++)
             mat[T][U]=rand()%50;
-        }
-    }
     for (int T=0;T<5;T++){
-        for(int U=0;U<5;U++){
+        for(int U=0;U<5;U++)
             cout<<mat[T][U]<<" ";
         cout<<endl;
         }
+        n1=mat[0][0];
+        ni=mat[0][0];
+    for(int T=0;T<5;T++){
+        k=0;
+        for(int U=0;U<5;U++)
+            k+=abs(mat[U][T]);
+            if(k>n1)
+                n1=k;
     }
+    for(int T=0;T<5;T++){
+        y=0;
+        for(int U=0;U<5;U++)
+            y+=abs(mat[T][U]);
+            if(y>ni)
+                ni=y;
+    }
+    cout<<"Norm1="<<n1<<" norm inf="<<ni<<endl;
     for(int T=0;T<5;T++){
         minim=mat[T][0];
         for(int U=0;U<5;U++){
@@ -28,17 +42,16 @@ int main(){
                 minim=mat[T][U];
                 m=U;
             }
-        if (T==U)
-            gl=mat[T][U];
+            if (T==U)
+                gl=mat[T][U];
         }
-            mat[T][m]=gl;
-            mat[T][T]=minim;
+        mat[T][m]=gl;
+        mat[T][T]=minim;
     }
     for (int T=0;T<5;T++){
-        for(int U=0;U<5;U++){
+        for(int U=0;U<5;U++)
             cout<<mat[T][U]<<" ";
-            cout<<endl;
-        }
+        cout<<endl;
     }
     return 0;
 }
