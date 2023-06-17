47a27b98bc8d28695f69595a12ca270bef6f1abd
xaskilaki007 <115955213+xaskilaki007@users.noreply.github.com>
2023-06-17 16:45:13 +0300

    Update 8.cpp

--- 8.cpp
+++ 8.cpp
@@ -6,26 +6,28 @@
 const int N = 5;
 int main()
 {
-    std::array<std::array<int, N>, 2> data;
+    std::array<std::array<int, N+1>, 2> data;
 
-    for(int i =0; i < 2; i++)
-        for(int j = 0; j < N; j++)
-            if(i)
+    for (int i = 0; i < 2; i++)
+        for (int j = 0; j < N; j++)
+            if (i)
                 data[i][j] = rand() % 3 + 1;
             else
                 data[i][j] = rand() % 50 + 10;
 
-    for(int i = 0; i < 2; i++){
-        for(int j = 0; j < N; j++)
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < N; j++){
             std::cout << data[i][j] << " ";
-            std::cout << std::endl;
+            if (data[i][j] < 10)std::cout << " ";
+        }
+        std::cout << std::endl;
     }
 
 
-
-    int max_index = -1;
-    for(int i = 0; i < N; i++)
-        if(data[0][i] > data[0][max_index] && data[1][i] == 3)
+    int max_index = N;
+    data[0][N] = -100;
+    for (int i = 0; i < N; i++)
+        if (data[0][i] > data[0][max_index] && data[1][i] == 3)
             max_index = i;
     std::cout << "Power: " << data[0][max_index] << std::endl;
 
